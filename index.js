5351f120a4a75387977b397d4b3775688e88acf1
Mekel Tensley <mekelintech@gmail.com>
2022-01-31 03:13:13 -0500

    Add routes

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 require('dotenv').config();
 const express = require("express");
-const app = express;
+const app = express();
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
-  console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
-}); vbhjio
+const PORT = process.env.PORT || 8000;
 
-module.exports = server;
+// Authentication Controllers
+
+const authRoute = require("./controllers/auth");
+
+//Mongoose Connection
+
+mongoose.connect(process.env.MONGO_URL)
+  .then (() => console.log("Connection Successful"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT`, PORT);
+});
